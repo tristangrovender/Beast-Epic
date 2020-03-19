a63fa33651dab3af8b313a5a9b954e19abf27e92
Tristan Grovender <therealtristangrovender@gmail.com>
2020-03-19 15:42:46 -0500

    Basic styled component button from docs

--- src/Learning.js
+++ src/Learning.js
@@ -1,9 +1,27 @@
 import React from "react";
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+
+    ${props =>
+        props.primary &&
+        css`
+            background: palevioletred;
+            color: white;
+        `};
+`;
 
 const Learning = () => {
     return (
         <div>
-            <h1>I'm learning!!!</h1>
+            <Button>Normal Button</Button>
+            <Button primary>Primary Button</Button>
         </div>
     );
 };
